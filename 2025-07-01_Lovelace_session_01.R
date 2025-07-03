336691515b3fbea42a619278ce5fe96f91c40b49
Brendan Clarke <brendan.clarke2@nhs.scot>
2025-07-03 10:57:26 +0100

    session 1 of lovelace

---
+++ 2025-07-01_Lovelace_session_01.R
@@ -0,0 +1,13 @@
+# Lovelace, Nowosad and Muenchow 2025 Geocomputation with R
+# https://r.geocompx.org/
+
+# main package loading for the book
+install.packages("sf")
+install.packages("terra")
+install.packages("spData")
+install.packages("spDataLarge", repos = "https://geocompr.r-universe.dev")
+
+# schedule - let's see how the first data session goes before committing
+read_csv("data/schedule.csv")
+
+
---
+++ 2025-07-08_Lovelace_session_02.R
@@ -0,0 +1,5 @@
+library(tidyverse)
+library(sf)
+library(terra)
+library(spData)
+library(spDataLarge)
