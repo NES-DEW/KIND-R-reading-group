06c9c5defdd839436d886eb9fe7a1472e9704ddd
Brendan Clarke <brendan.clarke2@nhs.scot>
2025-07-10 08:36:15 +0100

    pre-session 2

--- 2025-07-08_Lovelace_session_02.R
+++ 2025-07-08_Lovelace_session_02.R
@@ -1,4 +1,7 @@
+# this session = how far can we get with chapter 2?
+# https://r.geocompx.org/spatial-class
 
+# first impressions: equals assignment and lots of base-R plot()
 
 # packages ----
 # remotes::install_github("geocompx/geocompkg") # all dependencies
@@ -71,3 +74,44 @@ plot(st_geometry(world_cents), add = TRUE, cex = sqrt(world$pop) / 10000)
 india = world[world$name_long == "India", ]
 plot(st_geometry(india), col = "gray", expandBB = c(0, .2, .1, 1), lwd = 3)
 plot(st_geometry(world_asia), add = TRUE)
+
+
+# constructing an sf object ----
+
+lnd_point <- st_point(c(0.1, 51.5)) # sfg - simple feature geometry - object describing a point
+
+lnd_geom <- st_sfc(lnd_point, crs = "EPSG:4326") # convert the st_point into sfc - simple feature geometry column - by adding a CRS
+
+lnd_attrib = data.frame( # then make non-geographical data in a data.frame/tibble
+  name = "London",
+  temperature = 25,
+  date = as.Date("2023-06-21")
+)
+
+lnd_sf = st_sf(lnd_attrib, geometry = lnd_geom) # sf object = sfc + non-geography
+
+# or idiomatically with pipes
+st_point(c(0.1, 51.5)) |>
+  st_sfc(crs = "EPSG:4326") |>
+  st_sf(lnd_attrib, geometry = _)
+
+# matrixes!!
+rbind(c(1, 5), c(4, 4), c(4, 1), c(2, 2), c(3, 2)) |>
+  st_linestring()
+
+matrix(c(1,2,3,45,6,6,34,2,3,4,5,5), ncol = 2, byrow = T) # might be less annoying
+
+
+scottish_cities <- tribble(
+  ~ City,  ~ Latitude,  ~ Longitude,  ~ MeanAvgTempC,  ~ Population, 
+  "Glasgow",55.8642,-4.2518,8.5,632350,
+  "Edinburgh",55.9533,-3.1883,8.5,506520,
+  "Aberdeen",57.1497,-2.0943,8.0,198590,
+  "Dundee",56.4620,-2.9707,8.2,148210,
+  "Inverness",57.4778,-4.2247,7.8,47000,
+  "Stirling",56.1165,-3.9369,8.1,37910,
+  "Perth",56.3969,-3.4370,8.0,47000,
+  "Dunfermline",56.0717,-3.4521,8.2,54990
+) # data from rubbish copilot, which thinks that Cumbernauld is a city and doesn't understand how populations work
+
+
